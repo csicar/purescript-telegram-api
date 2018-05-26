199499f945015e1c4f46a0780d2fea42d2786527
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2018-05-27 00:31:55 +0200

    basic test added

--- test/Main.purs
+++ test/Main.purs
@@ -1,9 +1,21 @@
 module Test.Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "You should add some tests."
+import Control.Monad.Aff (Aff, launchAff_)
+import Control.Monad.Aff.Console (logShow)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE)
+import Network.HTTP.Affjax (AJAX)
+import Token (token)
+import Web.Telegram.API.Bot (ParsedUpdates, ApiOptions, defaultOptions, pollUpdates)
+
+opts :: ApiOptions
+opts = defaultOptions token
+
+main :: ∀e. Eff ( ajax :: AJAX, console :: CONSOLE| e) Unit
+main = launchAff_ $ pollUpdates opts onUpdate unit
+  where
+    onUpdate :: Unit → ParsedUpdates → Aff _ Unit
+    onUpdate _ updates = do
+      logShow updates
