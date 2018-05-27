20e31e26d0a114abb38e5d7fbf43771f7b057745
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2018-05-27 02:53:11 +0200

    updated tests

--- test/Main.purs
+++ test/Main.purs
@@ -6,12 +6,13 @@ import Control.Monad.Aff (Aff, launchAff_)
 import Control.Monad.Aff.Console (logShow)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE)
+import Data.Default (withRequired)
 import Network.HTTP.Affjax (AJAX)
 import Token (token)
-import Web.Telegram.API.Bot (ParsedUpdates, ApiOptions, defaultOptions, pollUpdates)
+import Web.Telegram.API.Bot (ParsedUpdates, ApiOptions, pollUpdates)
 
 opts :: ApiOptions
-opts = defaultOptions token
+opts = withRequired {token}
 
 main :: ∀e. Eff ( ajax :: AJAX, console :: CONSOLE| e) Unit
 main = launchAff_ $ pollUpdates opts onUpdate unit
